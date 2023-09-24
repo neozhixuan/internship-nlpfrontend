27a317bd71f69849f17e1ca0ea57ee80fdf38a3e
Zhi <neozhixuan@gmail.com>
2023-09-24 17:52:03 +0800

    Update query.tsx

--- src/services/query.tsx
+++ src/services/query.tsx
@@ -2,6 +2,6 @@ import http from "../http-common";
 
 export class QueryService {
   sendQuery(input: string) {
-    return http.get("/api/similarity", { query: input });
+    return http.post("/api/similarity", { query: input });
   }
 }
