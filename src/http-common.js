cdf42f7fa990dd45cb6fcdb3fe3807d46ea7c5a3
Zhi <neozhixuan@gmail.com>
2023-10-09 17:12:22 +0800

    Feature - Pull from fly.io

--- src/http-common.js
+++ src/http-common.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // axios.defaults.withCredentials = true;
 
 export default axios.create({
-  baseURL: "https://nlp-api-hroi.onrender.com/",
+  baseURL: "https://nlp-api.fly.dev/",
   headers: {
     "Content-type": "application/json",
   },
